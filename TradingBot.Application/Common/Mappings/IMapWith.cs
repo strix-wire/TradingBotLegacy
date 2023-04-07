a179b26f9b06081809bb16671ce0003e4338c79d
Vasily Pavlov <Pavlov.V@nppstels.ru>
2023-04-07 16:03:07 +0700

    Add mappings to TradingBot.Application

---
+++ TradingBot.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace TradingBot.Application.Common.Mappings
+{
+    public interface IMapWith<T>
+    {
+        //Create from type t - selected type
+        void Mapping(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
+    }
+}
