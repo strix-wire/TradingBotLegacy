d83200c181f078b57da7c0908853b5c406013f73
Vasily Pavlov <Pavlov.V@nppstels.ru>
2023-04-07 16:02:35 +0700

    create domain entity: Trade.cs, TradingAccount.cs

---
+++ TradingBot.Domain/Trade.cs
@@ -0,0 +1,20 @@
+﻿namespace TradingBot.Domain;
+
+public class Trade
+{
+    public Trade(string tradeId, string symbol, decimal volume, decimal price, DateTime openTime)
+    {
+        TradeId = tradeId;
+        Symbol = symbol;
+        Volume = volume;
+        Price = price;
+        OpenTime = openTime;
+    }
+    public string TradeId { get; set; } // идентификатор сделки
+    public string Symbol { get; set; } // символ инструмента, по которому проводилась сделка
+    public decimal Volume { get; set; } // объем сделки
+    public decimal Price { get; set; } // цена по которой была проведена сделка
+    public DateTime OpenTime { get; set; } // время открытия сделки
+    public DateTime? CloseTime { get; set; } // время закрытия сделки, null если сделка еще не закрыта
+    public decimal Profit { get; set; } // прибыль/убыток по сделке
+}
---
+++ TradingBot.Domain/TradingAccount.cs
@@ -0,0 +1,20 @@
+﻿namespace TradingBot.Domain;
+
+public class TradingAccount
+{
+    public TradingAccount(string accountId, string apiKey, string apiSecret)
+    {
+        AccountId = accountId;
+        ApiKey = apiKey;
+        ApiSecret = apiSecret;
+        Trades = new List<Trade>();
+    }
+    public string AccountId { get; set; } // идентификатор аккаунта
+    public string ApiKey { get; set; } // ключ API для доступа к торговой платформе
+    public string ApiSecret { get; set; } // секретный ключ API для доступа к торговой платформе
+    public decimal Balance { get; set; } // баланс на аккаунте
+    public decimal Equity { get; set; } // сумма всех открытых позиций на аккаунте
+    public decimal FreeMargin { get; set; } // доступная для торговли сумма
+    public decimal MarginLevel { get; set; } // уровень маржи на аккаунте
+    public List<Trade> Trades { get; set; } // список сделок на аккаунте
+}
