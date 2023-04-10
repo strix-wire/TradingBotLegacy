69dc247cc6f2a0cf7f7d701eef49f08d4bc12aa9
Vasily Pavlov <Pavlov.V@nppstels.ru>
2023-04-10 13:26:18 +0700

    add BaseStrategy.cs

---
+++ TradingBot.Strategies/Strategies/BaseStrategy.cs
@@ -0,0 +1,16 @@
+﻿using TradingBot.Application.Interfaces;
+
+namespace TradingBot.Strategies.Strategies;
+
+public abstract class BaseStrategy
+{
+    protected IExchangeApiClient ExchangeApiClient { get; set; }
+
+    public BaseStrategy(IExchangeApiClient exchangeApiClient)
+        => ExchangeApiClient = exchangeApiClient;
+
+    /// <summary>
+    /// Запускает работу
+    /// </summary>
+    public abstract string Run { get; }
+}
