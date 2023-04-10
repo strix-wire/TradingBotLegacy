8a0fefee94b56e4147d5b380c5da6bccdf104812
Vasily Pavlov <Pavlov.V@nppstels.ru>
2023-04-10 15:09:00 +0700

    add template LevelAndGlassStrategy.cs

---
+++ TradingBot.Strategies/Strategies/LevelAndGlassStrategy/LevelAndGlassStrategy.cs
@@ -0,0 +1,18 @@
+﻿using TradingBot.Application.Interfaces;
+
+namespace TradingBot.Strategies.Strategies.LevelAndGlassStrategy;
+
+internal class LevelAndGlassStrategy : BaseStrategy
+{
+    public LevelAndGlassStrategy(IExchangeApiClient exchangeApiClient) : base(exchangeApiClient)
+    {
+    }
+
+    public override void Run()
+    {
+        while (true)
+        {
+            
+        }
+    }
+}
