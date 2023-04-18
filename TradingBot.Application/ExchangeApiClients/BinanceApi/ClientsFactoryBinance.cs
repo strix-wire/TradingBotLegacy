2dcae720797b7eb14400ed82ae68b546b17c64af
Vasily Pavlov <Pavlov.V@nppstels.ru>
2023-04-18 14:43:14 +0700

    improve ClientsFactoryBinance

--- TradingBot.Application/ExchangeApiClients/BinanceApi/ClientsFactoryBinance.cs
+++ TradingBot.Application/ExchangeApiClients/BinanceApi/ClientsFactoryBinance.cs
@@ -10,23 +10,52 @@ public class ClientsFactoryBinance
     private static string _apiKey { get; set; }
     private static string _apiSecret { get; set; }
     public ClientsFactoryBinance() => 
-        (ConfigurationManager.AppSettings["apiKey"], ConfigurationManager.AppSettings["apiSecret"]) = (_apiKey, _apiSecret);
+        (ConfigurationManager.AppSettings["apiKeyBinance"], ConfigurationManager.AppSettings["apiSecretBinance"]) = (_apiKey, _apiSecret);
 
-    public static IExchangeApiClient GetFactory(string factoryType)
+    public static IExchangeApiClient GetClient(TypeBinanceClient typeBinanceClient)
     {
-        switch (factoryType)
+        switch (typeBinanceClient)
         {
-            case "USDFutures":
-            {
-                var clientHttp = new BinanceClient();
-                clientHttp.SetApiCredentials(CreateApiCredentials(_apiKey, _apiSecret));
-                var socketClient = new BinanceSocketClient();
-
-                return new BinanceUsdFuturesApiClient(clientHttp.UsdFuturesApi, socketClient.UsdFuturesStreams);
-            }
+            case TypeBinanceClient.USDFutures:
+                return CreateBinanceUSDFutures();
+            case TypeBinanceClient.USDSpot:
+                return CreateBinanceUSDSpot();
+            
             default: { throw new ArgumentException("Invalid factory type"); }
         }
     }
     private static BinanceApiCredentials CreateApiCredentials(string apiKey, string apiSecret)
         => new BinanceApiCredentials(apiKey, apiSecret);
+
+    private static AllBinanceClients CreateBinanceClient()
+    {
+        var clientHttp = new BinanceClient();
+        clientHttp.SetApiCredentials(CreateApiCredentials(_apiKey, _apiSecret));
+        var socketClient = new BinanceSocketClient();
+
+        return new AllBinanceClients(clientHttp, socketClient);
+    }
+    private static BinanceUsdFuturesApiClient CreateBinanceUSDFutures()
+    {
+        var allBinanceClients = CreateBinanceClient();
+        
+        return new BinanceUsdFuturesApiClient(allBinanceClients.ClientHttp.UsdFuturesApi,
+            allBinanceClients.SocketClient.UsdFuturesStreams);
+    }
+    private static BinanceUsdSpotApiClient CreateBinanceUSDSpot()
+    {
+        var allBinanceClients = CreateBinanceClient();
+        
+        return new BinanceUsdSpotApiClient(allBinanceClients.ClientHttp.SpotApi,
+            allBinanceClients.SocketClient.SpotStreams);
+    }
+}
+
+internal class AllBinanceClients
+{
+    public AllBinanceClients(BinanceClient client, BinanceSocketClient socketClient)
+        => (ClientHttp, SocketClient) = (client, socketClient);
+
+    public BinanceClient ClientHttp { get; set; }
+    public BinanceSocketClient SocketClient { get; set; }
 }
